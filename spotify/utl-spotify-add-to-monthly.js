2cfc9bc055d1cd5c707d18ff09b00c526687c170
Darren Packer <darren.d.packer@gmail.com>
2026-05-27 20:42:13 -0400

    feat(spotify): create utility to add the current track to the monthly playlist

---
+++ spotify/utl-spotify-add-to-monthly.js
@@ -0,0 +1,30 @@
+const Spotify = importModule('spotify');
+const spotify = new Spotify();
+const alert = new Alert();
+
+(async () => {
+    const currentlyPlaying = await spotify.getCurrentlyPlaying();
+    const trackUri = currentlyPlaying?.item?.uri;
+
+    if (trackUri) {
+        const playlist = await spotify.getMonthlyPlaylist();
+        if (playlist) {
+            const isInPlaylist = await spotify.isSongInPlaylist(playlist, trackUri);
+            if (isInPlaylist) {
+                alert.title = "Already in Monthly Playlist";
+                alert.message = `${currentlyPlaying.item.name} is already in ${playlist.name}`;
+                alert.addAction("OK");
+                await alert.present();
+                return;
+            } else {
+                const result = await spotify.addToPlaylist(playlist.id, [trackUri]);
+                if (result) {
+                    alert.title = "Added to Monthly Playlist";
+                    alert.message = `Added ${currentlyPlaying.item.name} to ${playlist.name}`;
+                    alert.addAction("OK");
+                    await alert.present();
+                }
+            }
+        }
+    }
+});
