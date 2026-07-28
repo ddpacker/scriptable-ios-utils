17616d6b8fc78ef585d694e59ab6498f4521218c
Darren Packer <darren.d.packer@gmail.com>
2026-07-28 18:28:33 -0400

    chore: remove dist in favor of manual file drops

--- dist/utl-spotify-auth.js
+++
@@ -1,8 +0,0 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: red; icon-glyph: magic;
-(async () => {
-    const Spotify = importModule('spotify');
-    const _spotify = new Spotify();
-    await _spotify.login();
-})();
